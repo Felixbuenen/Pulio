085e86741028bdcffdc8d46128f1797e7c91acad
Felix Buenen <felixbuenen@hotmail.com>
2020-12-26 13:13:20 +0100

    add assertion macro

--- Pulio/src/Pulio/Core/Core.h
+++ Pulio/src/Pulio/Core/Core.h
@@ -14,6 +14,27 @@
 // helper to set xth bit
 #define BIT(x) 1 << x
 
+// debug break helper
+#ifdef PL_PLATFORM_WINDOWS
+#define PL_DEBUG_BREAK DebugBreak()
+#else
+#error Pulio only supports windows!
+#endif
+
+// assertion helper
+// TODO: add option to enable/disable asserts
+// TODO: add a macro to print 'assertion failed' log
+#if PL_DEBUG
+#define PL_ASSERT(condition, errorMsg, ...) { \
+	if(!(condition)) { \
+		PULIO_LOG_ERROR(errorMsg, __VA_ARGS__); \
+		PL_DEBUG_BREAK; \
+	} \
+}
+#else
+#define PL_ASSERT(...)
+#endif
+
 // helper to bind functions
 #define FN_BIND_1_ARG(cls, fn)  std::bind(&cls::fn, this, std::placeholders::_1)
 #define FN_BIND_2_ARG(cls, fn)  std::bind(&cls::fn, this, std::placeholders::_2)
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -8,6 +8,8 @@ public:
 	Sandbox() {
 		PULIO_LOG_INFO("Created the sandbox application.");
 
+		PL_ASSERT(2 < 1, "test message {0}", 1);
+
 		PushLayer(new TestLayer("Test Layer"));
 	}
 
